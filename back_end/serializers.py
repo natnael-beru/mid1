8340965ca4a7b9743822a9bb2547c6a2f4d734be
natnael-beru <120000938+natnael-beru@users.noreply.github.com>
2023-01-26 06:54:48 -0800

    Update serializers.py

--- back_end/serializers.py
+++ back_end/serializers.py
@@ -2,12 +2,7 @@ from django.db import models
 from django.db.models import fields
 from rest_framework import serializers
 from .models import Student_information
-
-
-
 class studentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student_information
         fields = '__all__'
-
-    
